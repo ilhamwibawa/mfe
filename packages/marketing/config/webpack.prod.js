d3b0c97ff7dfe29da8793196ddc5222da830aeec
Ilham Wibawa <ilham@smartm2m.co.kr>
2023-01-04 13:21:30 +0700

    Created container deploy script

---
+++ packages/marketing/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require('webpack-merge')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
+const deps = require('../package.json').dependencies
+const commonConfig = require('./webpack.common')
+
+const prodConfig = {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'marketing',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './MarketingApp': './src/bootstrap'
+      },
+      shared: deps
+    })
+  ]
+}
+
+module.exports = merge(commonConfig, prodConfig)
